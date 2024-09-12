c00624713118c76f804fb2b9ac5f2cf7165f94d9
DuVitoretiSantos <du16062006@hotmail.com>
2024-09-12 16:14:33 -0300

    Inicio home

---
+++ RentWorkers FRONTEND/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import NavBar from './componentes/TelaHome/NavBar';
+import Body from './componentes/TelaHome/Body';
+
+function App() {
+
+  return (
+    <div>
+       <NavBar />
+       <Body />
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ RentWorkers FRONTEND/src/componentes/TelaHome/NavBar.jsx
@@ -0,0 +1,27 @@
+
+import '../TelaHome/NavBar.css';
+
+function NavBar() {
+
+    function indoParaLogin() {
+        alert("To vendo sobre react router logo logo atualizo e boto isso aqui pra funcionar...");
+    }
+
+    return (
+      <div className="navContainer">
+        <div className="navLogo">
+          <img src="/images/LogoRentWorkers.png" alt='' width={150} height={100}/>    
+        </div>
+        <div className='navButtons'>
+          <p>Home</p>
+          <p>Sobre nós</p>
+          <p>Serviços</p>
+          <p>Como funciona</p>
+          <button className='entrarButton' onClick={ indoParaLogin }>Entrar</button>
+        </div>
+
+      </div>
+    )
+}
+
+export default NavBar;
