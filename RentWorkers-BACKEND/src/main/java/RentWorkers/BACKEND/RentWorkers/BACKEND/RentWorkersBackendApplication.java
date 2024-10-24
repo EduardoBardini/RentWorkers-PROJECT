28a6748184b772953b76019fc2a01db0abab6eab
EDUARDO BARDINI VITORETI DOS SANTOS <eduardo_bv_santos@estudante.sesisenai.org.br>
2024-10-23 21:58:30 -0300

    Back Adicionado

---
+++ RentWorkers-BACKEND/src/main/java/RentWorkers/BACKEND/RentWorkers/BACKEND/RentWorkersBackendApplication.java
@@ -0,0 +1,13 @@
+package RentWorkers.BACKEND.RentWorkers.BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentWorkersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentWorkersBackendApplication.class, args);
+	}
+
+}
---
+++ RentWorkers-BACKEND/src/test/java/RentWorkers/BACKEND/RentWorkers/BACKEND/RentWorkersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package RentWorkers.BACKEND.RentWorkers.BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentWorkersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
